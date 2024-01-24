e81745459b3c3b4e953ba56c953c23adf6c323ac
glezbryan <bryan.gonzalez2@mavs.uta.edu>
2024-01-23 22:04:47 -0600

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-    const menu_button =document.querySelector('.hamburger');
-    const mobile_menu = document.querySelector('.mobile-nav')
+    const menu_button = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
 
     menu_button.addEventListener('click', function(){
         menu_button.classList.toggle('is-active');
